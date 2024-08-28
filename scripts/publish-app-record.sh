d6faabce11823b4ffd16aed2fc78ad6e7b62c9ef
ac9.xyz <ac9.xyz@yahoo.com>
2024-08-27 21:30:43 -0500

    Deploying GitHub Action

--- scripts/publish-app-record.sh
+++ scripts/publish-app-record.sh
@@ -18,21 +18,23 @@ rcd_license=$(jq -r '.license' package.json | sed 's/null//')
 rcd_author=$(jq -r '.author' package.json | sed 's/null//')
 rcd_app_version=$(jq -r '.version' package.json | sed 's/null//')
 
+CERC_REGISTRY_DEPLOYMENT_REQUEST_PAYMENT_TO="laconic1qsvrehuchjquanwm7gsx7cvze8t959l7zf0zpw"
+
 cat <<EOF > "$CONFIG_FILE"
 services:
   registry:
     rpcEndpoint: '${CERC_REGISTRY_REST_ENDPOINT:-https://laconicd.laconic.com}'
     gqlEndpoint: '${CERC_REGISTRY_GQL_ENDPOINT:-https://laconicd.laconic.com/api}'
     chainId: ${CERC_REGISTRY_CHAIN_ID:-laconic_9000-1}
-    gas: 9550000
-    fees: 15000000alnt
+    gas: 150000
+    fees: 150000alnt
 EOF
 
-if [ -z "$CERC_REGISTRY_BOND_ID" ]; then
-  bond_id=$(laconic -c $CONFIG_FILE registry bond create --type alnt --quantity 10000000000 --user-key "${CERC_REGISTRY_USER_KEY}")
-
-  CERC_REGISTRY_BOND_ID=$(echo ${bond_id} | jq -r .bondId)
-fi
+#if [ -z "$CERC_REGISTRY_BOND_ID" ]; then
+#  bond_id=$(laconic -c $CONFIG_FILE registry bond create --type alnt --quantity 10000000000 --user-key "${CERC_REGISTRY_USER_KEY}")
+#
+#  CERC_REGISTRY_BOND_ID=$(echo ${bond_id} | jq -r .bondId)
+#fi
 
 next_ver=$(laconic -c $CONFIG_FILE registry record list --type ApplicationRecord --all --name "$rcd_name" 2>/dev/null | jq -r -s ".[] | sort_by(.createTime) | reverse | [ .[] | select(.bondId == \"$CERC_REGISTRY_BOND_ID\") ] | .[0].attributes.version" | awk -F. -v OFS=. '{$NF += 1 ; print}')
 
@@ -65,7 +67,7 @@ if [ -z "$CERC_REGISTRY_APP_CRN" ]; then
   authority=$(echo "$rcd_name" | cut -d'/' -f1 | sed 's/@//')
   app=$(echo "$rcd_name" | cut -d'/' -f2-)
   CERC_REGISTRY_APP_CRN="lrn://$authority/applications/$app"
-  laconic -c $CONFIG_FILE registry authority reserve ${authority} --user-key "${CERC_REGISTRY_USER_KEY}"
+#  laconic -c $CONFIG_FILE registry authority reserve ${authority} --user-key "${CERC_REGISTRY_USER_KEY}"
   laconic -c $CONFIG_FILE registry authority bond set ${authority} ${CERC_REGISTRY_BOND_ID} --user-key "${CERC_REGISTRY_USER_KEY}"
 fi
 
@@ -75,6 +77,12 @@ if [ "true" == "$CERC_IS_LATEST_RELEASE" ]; then
   laconic -c $CONFIG_FILE registry name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN" "$AR_RECORD_ID"
 fi
 
+#PAYMENT_TX=$(laconic -c $CONFIG_FILE registry tokens send \
+#  --address $CERC_REGISTRY_DEPLOYMENT_REQUEST_PAYMENT_TO \
+#  --user-key "${CERC_REGISTRY_DEPLOYMENT_REQUEST_USER_KEY}" \
+#  --bond-id "${CERC_REGISTRY_DEPLOYMENT_REQUEST_BOND_ID}" \
+#  --type alnt \
+#  --quantity ${CERC_REGISTRY_DEPLOYMENT_REQUEST_PAYMENT_AMOUNT:-10000} | jq '.tx.hash')
 
 APP_RECORD=$(laconic -c $CONFIG_FILE registry name resolve "$CERC_REGISTRY_APP_CRN" | jq '.[0]')
 if [ -z "$APP_RECORD" ] || [ "null" == "$APP_RECORD" ]; then
@@ -90,6 +98,8 @@ record:
   application: "$CERC_REGISTRY_APP_CRN@$rcd_app_version"
   dns: "$CERC_REGISTRY_DEPLOYMENT_SHORT_HOSTNAME"
   deployment: "$CERC_REGISTRY_DEPLOYMENT_CRN"
+  to: $CERC_REGISTRY_DEPLOYMENT_REQUEST_PAYMENT_TO
+  payment: $PAYMENT_TX
   config:
     env:
       CERC_WEBAPP_DEBUG: "$rcd_app_version"
